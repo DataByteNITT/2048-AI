b85f47b5bd3b2dd28e4bcc46ef1e4b99d59a9e64
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2017-03-13 23:09:15 +0530

    Minor code changes

--- server/server.py
+++ server/server.py
@@ -5,20 +5,30 @@ import websockets
 import json
 #import getch
 
+
 async def hello(websocket, path):
-		
-		while True:
-			
-			print("get inp")
-			greeting = input()
-			await websocket.send(str(greeting))
-			print("> {}".format(greeting))
-			
-			resp = await websocket.recv()
-			data = json.loads(resp)            
-			print("Recieved Game State : {}".format(data))
+
+    while True:
+
+        print("get inp")
+        # greeting =send()
+        # await websocket.send(str(greeting))
+        # print("> {}".format(greeting))
+
+        message = await websocket.recv()
+        await consumer(message)
+        # data = json.loads(resp)
+        print("Recieved Game State : {}".format(data))
 start_server = websockets.serve(hello, 'localhost', 9876)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
+def consumer(message):
+    print ("GOT YOUR MESSAGE")
+def send(data):
+    pass
+
+
+def receive():
+    pass
