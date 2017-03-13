4151c123096c2ea060b236ef3b8d92f936f5b6b1
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2017-03-13 21:59:31 +0530

    Updated Server Module to read game state over the network

--- server/server.py
+++ server/server.py
@@ -2,20 +2,21 @@
 
 import asyncio
 import websockets
-import getch
+import json
+#import getch
 
 async def hello(websocket, path):
-        
-        while True:
-            
-            print("get inp")
-            greeting = getch.getch()
-            await websocket.send(str(greeting))
-            print("> {}".format(greeting))
-            
-            resp = await websocket.recv()
-            print(resp)
-
+		
+		while True:
+			
+			print("get inp")
+			greeting = input()
+			await websocket.send(str(greeting))
+			print("> {}".format(greeting))
+			
+			resp = await websocket.recv()
+			data = json.loads(resp)            
+			print("Recieved Game State : {}".format(data))
 start_server = websockets.serve(hello, 'localhost', 9876)
 
 asyncio.get_event_loop().run_until_complete(start_server)
